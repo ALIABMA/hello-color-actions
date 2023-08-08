38595c577f53aafbbf554e4f3a6d2a3babeb96d5
ALIABMA <ALIABMA@hotmail.com>
2023-08-08 12:29:16 -0500

    #

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola, Mundo, desde Spagna");
+    expect(hello.hello()).toEqual("Hola, Mundo, desde Espagna");
   });
 });
